ed05d6e61b0f8be2f9012280e0077af9a8db4baf
Tangeni Shikomba <tangenishikomba@gmail.com>
2018-08-05 02:49:43 +0200

    error handling added

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Post} from '../post.model';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-post-create',
@@ -15,10 +16,13 @@ export class PostCreateComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
     // console.dir(postInput)
-    const post = {title: this.enteredTitle,
-                  content: this.enteredContent};
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {title: form.value.title,
+                  content: form.value.content};
     this.postCreated.emit(post);
   }
 
